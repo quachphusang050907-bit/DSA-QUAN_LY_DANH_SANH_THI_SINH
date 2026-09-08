d27742662b03b0f84bb911cc917d73791c9afaa3
quachphusang0509 <quachphusang.050907@gmail.com>
2026-09-08 19:18:30 +0700

    Update Candidate.h

--- Candidate.h
+++ Candidate.h
@@ -1,1 +1,16 @@
+#ifndef CANDIDATE_H
+#define CANDIDATE_H
 
+#include <string>
+
+using namespace std;
+
+struct Candidate {
+    string fullName;
+    string gender;
+    string examID;
+    string birthDate;
+    string hometown;
+};
+
+#endif
