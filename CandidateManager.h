e382ab240f54bf25258117b33f305728dca1dc15
quachphusang0509 <quachphusang.050907@gmail.com>
2026-09-08 19:18:57 +0700

    Update CandidateManager.h

--- CandidateManager.h
+++ CandidateManager.h
@@ -1,1 +1,34 @@
+#ifndef CANDIDATE_MANAGER_H
+#define CANDIDATE_MANAGER_H
 
+#include "HashTable.h"
+#include "CandidateSorter.h"
+#include "GroupManager.h"
+#include "RoomManager.h"
+#include "FileManager.h"
+
+class CandidateManager {
+private:
+    HashTable hashTable;
+
+public:
+    bool addCandidate(const Candidate& candidate);
+
+    Candidate* searchCandidate(const string& examID);
+
+    bool removeCandidate(const string& examID);
+
+    vector<Candidate> getAllCandidates() const;
+
+    vector<Candidate> getSortedCandidates() const;
+
+    vector<vector<Candidate>> groupCandidates() const;
+
+    vector<Room> divideCandidates(int numberOfRooms);
+
+    bool saveData(const string& filename);
+
+    bool loadData(const string& filename);
+};
+
+#endif
